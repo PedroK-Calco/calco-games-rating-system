5969c594de2a8387a08ac5da6a96788bf53b813c
PedroK-Calco <132271856+PedroK-Calco@users.noreply.github.com>
2023-08-09 14:26:48 +0200

    added variable g_rd and calculation method as I understand it

--- service/glicko.py
+++ service/glicko.py
@@ -6,6 +6,7 @@ NEW_DEVIATION: int = 350
 
 def calculate_rating(ratings: int) -> int:
     q: float = calculate_q()
+    g_rd: float = calculate_g_rd()
     rd: int = 0
     d: float = calculate_d()
     sum_r: float = calculate_sum_r()
@@ -19,9 +20,15 @@ def calculate_deviation() -> int:
 
 
 def calculate_q() -> float:
-    q = log(10) / 400
+    q: float = log(10) / 400
 
     return q
+
+
+def calculate_g_rd(q: float, rd: int) -> float:
+    g_rd: float = 1 / sqrt(1 + (3 * (q ** 2)) * (rd ** 2) / (pi ** 2))
+
+    return g_rd
 
 
 def calculate_d() -> float:
