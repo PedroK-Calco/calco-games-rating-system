1c5f67e4dfd5052617c4a2250df7d947491f73fa
PedroK-Calco <132271856+PedroK-Calco@users.noreply.github.com>
2023-08-25 10:32:13 +0200

    Minor GUI updates for easy of use

--- presentation/gui.py
+++ presentation/gui.py
@@ -3,6 +3,10 @@ from service import PlayerService, MatchService
 from popo import Player, Match
 
 
+def _on_focus(e):
+    e.widget.delete(0, END)
+
+
 class GUI(Tk):
     def __init__(self, player_service: PlayerService, match_service: MatchService):
         super().__init__()
@@ -18,10 +22,12 @@ class GUI(Tk):
 
         self._player_1_entry = Entry(master=self._match_maker)
         self._player_1_entry.insert(0, "Player 1 ID")
+        self._player_1_entry.bind("<FocusIn>", _on_focus)
         self._player_1_entry.grid(row=1, column=0)
 
         self._player_2_entry = Entry(master=self._match_maker)
         self._player_2_entry.insert(0, "Player 2 ID")
+        self._player_2_entry.bind("<FocusIn>", _on_focus)
         self._player_2_entry.grid(row=1, column=1)
 
         Button(master=self._match_maker, text="Create Match", command=self._create_match).grid(row=2, column=0,
@@ -34,6 +40,7 @@ class GUI(Tk):
 
         self._match_id_entry = Entry(master=self._match_finder)
         self._match_id_entry.insert(0, "Match ID")
+        self._match_id_entry.bind("<FocusIn>", _on_focus)
         self._match_id_entry.grid(row=1, column=0, columnspan=2)
 
         Button(master=self._match_finder, text="Find Match", command=self._find_match).grid(row=2, column=0,
@@ -68,6 +75,9 @@ class GUI(Tk):
 
         self._match_ser.create_match(player_1, player_2)
 
+        self._player_1_entry.insert(0, "Player 1 ID")
+        self._player_2_entry.insert(0, "Player 2 ID")
+
     def _find_match(self):
         """
         Retrieves a Match object from the match repository and assigns it GUI's match attribute
@@ -98,3 +108,7 @@ class GUI(Tk):
                 self._player_ser.update_player(k, v)
 
             self._player_ser.save_player_data()
+
+            self._match_id_entry.delete(0, END)
+            self._player_1_radio.config(text="Player 1")
+            self._player_2_radio.config(text="Player 2")
